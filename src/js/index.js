809e62e86967ca5385283bf02596b58e4be4a685
ArkadiiDavydenko <80512176+ArkadiiDavydenko@users.noreply.github.com>
2022-05-09 17:08:18 +0300

    feat: add functions for entering text from the keyboard and mouse

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,7 @@ class Element {
 }
 
 const KEY_ENG = [
-    ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Backspace',],
+    ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',],
     ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'],
     ['Caps lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '"', 'Enter'],
     ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '?', '▲', 'Shift'],
@@ -17,11 +17,19 @@ const KEY_ENG = [
 ];
 
 const KEY_RUS = [
-    ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Backspace',],
+    ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',],
     ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', , 'х', 'ъ', '\\',],
     ['Caps lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter',],
     ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '▲', 'Shift',],
     ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', '◄', '▼', '►', 'Ctrl']
+];
+
+const KEY_CODE_TABLE = [
+    'Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace',
+    'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash',
+    'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter',
+    'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight',
+    'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'
 ];
 
 function init() {
@@ -32,15 +40,13 @@ function init() {
     TITLE.node.textContent = 'Виртуальная клавиатура';
 
     const MAIN = new Element(WRAPPER.node, 'main', 'main');
-    const TEXTAREA = new Element(MAIN.node, 'textarea', 'textarea');
-    TEXTAREA.node.setAttribute("rows", 5);
-    TEXTAREA.node.setAttribute("columns", 50);
-    // TEXTAREA.node.setAttribute("autofocus", );
+    MAIN.node.innerHTML = `<textarea class = "textarea" rows="5" cols="50" disabled></textarea>`
+    const TEXTAREA = document.querySelector('.textarea');
 
     const KEYBOARD = new Element(MAIN.node, 'div', 'keyboard');
 
     const DESCRIPTION = new Element(MAIN.node, 'p', 'description');
-    DESCRIPTION.node.textContent = 'Клавиатура создана в операционной системе Windows';
+    DESCRIPTION.node.textContent = 'Клавиатура создана для операционной системе Windows';
     const LANGUAGE = new Element(MAIN.node, 'p', 'language');
     LANGUAGE.node.textContent = 'Для переключения языка комбинация: левыe ctrl + alt';
 
@@ -74,14 +80,50 @@ function init() {
                 case 'Space':
                     keyButton.node.classList.add('keyboard__key_extra-long')
                     break
-
             }
         })
-
+    })
+    //Вывод текста в textarea
+    document.querySelectorAll('.keyboard__key').forEach(element => {
+        element.addEventListener('click', (e) => {
+            TEXTAREA.value += element.textContent;
+        })
 
     })
 
+    //Добавление класса
+    function addClass(arr) {
+        let elements = document.querySelectorAll('.keyboard__key');
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].dataset.keycode = `${arr[i]}`;
+        }
+    }
+
+    addClass(KEY_CODE_TABLE);
+
+    //События на клавиатуре
+    window.addEventListener('keydown', function (event) {
+        let elements = document.querySelectorAll('.keyboard__key');
+        for (let i = 0; i < elements.length; i++) {
+            if (elements[i].dataset.keycode === `${event.code}`) {
+                elements[i].classList.add('keyboard__key_active');
+                TEXTAREA.value += elements[i].textContent;
+
+            }
+        }
+    });
+
+    window.addEventListener('keyup', function (event) {
+        let elements = document.querySelectorAll('.keyboard__key');
+        for (let i = 0; i < elements.length; i++) {
+            if (elements[i].dataset.keycode === `${event.code}`) {
+                elements[i].classList.remove('keyboard__key_active')
+            }
+        }
+    });
+
 }
+
 
 window.addEventListener('DOMContentLoaded', init)
 
