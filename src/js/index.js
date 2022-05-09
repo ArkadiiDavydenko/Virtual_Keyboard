75f8de53d4fae5338a7569dc2daffb8340bc04c8
ArkadiiDavydenko <80512176+ArkadiiDavydenko@users.noreply.github.com>
2022-05-09 20:51:13 +0300

    feat: add functions for changing letter-register and functions for tab, enter and space keys

--- src/js/index.js
+++ src/js/index.js
@@ -17,11 +17,11 @@ const KEY_ENG = [
 ];
 
 const KEY_RUS = [
-    ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',],
-    ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', , 'х', 'ъ', '\\',],
-    ['Caps lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter',],
-    ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '▲', 'Shift',],
-    ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', '◄', '▼', '►', 'Ctrl']
+    '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
+    'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', , 'х', 'ъ', '\\',
+    'Caps lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter',
+    'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '▲', 'Shift',
+    'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '◄', '▼', '►', 'Ctrl'
 ];
 
 const KEY_CODE_TABLE = [
@@ -33,6 +33,9 @@ const KEY_CODE_TABLE = [
 ];
 
 function init() {
+
+    let upperLetter = false;
+    let engLanguage = true;
 
     const WRAPPER = new Element(document.body, 'div', 'wrapper');
     const HEADER = new Element(WRAPPER.node, 'header', 'header');
@@ -59,36 +62,35 @@ function init() {
             switch (key) {
                 case 'Backspace':
                     keyButton.node.classList.add('keyboard__key_very-long')
+                    keyButton.node.classList.add('backspace')
                     break
 
                 case 'Tab':
                     keyButton.node.classList.add('keyboard__key_long')
+                    keyButton.node.classList.add('tab')
                     break
 
                 case 'Shift':
                     keyButton.node.classList.add('keyboard__key_long')
+                    keyButton.node.classList.add('shift')
                     break
 
                 case 'Enter':
                     keyButton.node.classList.add('keyboard__key_long')
+                    keyButton.node.classList.add('enter')
                     break
 
                 case 'Caps lock':
                     keyButton.node.classList.add('keyboard__key_long')
+                    keyButton.node.classList.add('caps-lock')
                     break
 
                 case 'Space':
                     keyButton.node.classList.add('keyboard__key_extra-long')
+                    keyButton.node.classList.add('space')
                     break
             }
         })
-    })
-    //Вывод текста в textarea
-    document.querySelectorAll('.keyboard__key').forEach(element => {
-        element.addEventListener('click', (e) => {
-            TEXTAREA.value += element.textContent;
-        })
-
     })
 
     //Добавление класса
@@ -99,21 +101,110 @@ function init() {
         }
     }
 
+
     addClass(KEY_CODE_TABLE);
+
+
+    function useKey(element) {
+        switch (element) {
+            case 'Tab':
+                TEXTAREA.value += '    ';
+                break
+            case 'ShiftLeft':
+                if (!upperLetter) {
+                    document.querySelectorAll('.keyboard__key').forEach(element => {
+                        if (element.textContent.length < 2) element.textContent = element.textContent.toUpperCase()
+                    })
+                    upperLetter = true;
+                } else {
+                    document.querySelectorAll('.keyboard__key').forEach(element => {
+                        if (element.textContent.length < 2) element.textContent = element.textContent.toLowerCase();
+                    })
+                    upperLetter = false;
+                }
+                break
+            case 'ShiftRight':
+                if (!upperLetter) {
+                    document.querySelectorAll('.keyboard__key').forEach(element => {
+                        if (element.textContent.length < 2) element.textContent = element.textContent.toUpperCase()
+                    })
+                    upperLetter = true;
+                } else {
+                    document.querySelectorAll('.keyboard__key').forEach(element => {
+                        if (element.textContent.length < 2) element.textContent = element.textContent.toLowerCase();
+                    })
+                    upperLetter = false;
+                }
+                break
+            case 'Enter':
+                TEXTAREA.value += '\n';
+                break
+            case 'Space':
+                TEXTAREA.value += ' ';
+                break
+        }
+    }
+
+    function deleteElement (element) {
+        if (element === 'Backspace') {
+            console.log(TEXTAREA.value.length)
+            TEXTAREA.value =  TEXTAREA.value.substring(0, TEXTAREA.value.length - 1);
+        }
+    }
+
+    function capsLock(element) {
+        if (element === 'CapsLock') {
+
+            if (!upperLetter) {
+                document.querySelectorAll('.keyboard__key').forEach(element => {
+                    if (element.textContent.length < 2) element.textContent = element.textContent.toUpperCase()
+                })
+                upperLetter = true;
+            } else {
+                document.querySelectorAll('.keyboard__key').forEach(element => {
+                    if (element.textContent.length < 2) element.textContent = element.textContent.toLowerCase();
+                })
+                upperLetter = false;
+            }
+        }
+    }
+
+
+    //Вывод текста в textarea
+    document.querySelectorAll('.keyboard__key').forEach(element => {
+        element.addEventListener('mousedown', (e) => {
+            useKey(element.dataset.keycode);
+            capsLock(element.dataset.keycode);
+            deleteElement (element.dataset.keycode);
+            if (element.textContent.length < 2) {
+                TEXTAREA.value += element.textContent;
+            }
+        })
+    })
+
+    document.querySelectorAll('.keyboard__key').forEach(element => {
+        element.addEventListener('mouseup', (e) => {
+            useKey(element.dataset.keycode);
+        })
+    })
 
     //События на клавиатуре
     window.addEventListener('keydown', function (event) {
+        useKey(event.code);
+        capsLock(event.code);
+        deleteElement (event.code);
         let elements = document.querySelectorAll('.keyboard__key');
         for (let i = 0; i < elements.length; i++) {
+
             if (elements[i].dataset.keycode === `${event.code}`) {
                 elements[i].classList.add('keyboard__key_active');
-                TEXTAREA.value += elements[i].textContent;
-
+                if (elements[i].textContent.length < 2) TEXTAREA.value += elements[i].textContent;
             }
         }
     });
 
     window.addEventListener('keyup', function (event) {
+        useKey(event.code);
         let elements = document.querySelectorAll('.keyboard__key');
         for (let i = 0; i < elements.length; i++) {
             if (elements[i].dataset.keycode === `${event.code}`) {
@@ -125,6 +216,6 @@ function init() {
 }
 
 
-window.addEventListener('DOMContentLoaded', init)
+window.addEventListener('DOMContentLoaded', init);
 
 
