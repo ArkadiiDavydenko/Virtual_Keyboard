e0de391341e7e45dd9fe66a01f9525095997fe67
ArkadiiDavydenko <80512176+ArkadiiDavydenko@users.noreply.github.com>
2022-05-09 15:05:09 +0300

    feat: add animation for keys

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,6 @@ class Element {
     }
 }
 
-
 const KEY_ENG = [
     ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Backspace',],
     ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'],
@@ -25,57 +24,65 @@ const KEY_RUS = [
     ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', '◄', '▼', '►', 'Ctrl']
 ];
 
+function init() {
 
-const WRAPPER = new Element(document.body, 'div', 'wrapper');
-const HEADER = new Element(WRAPPER.node, 'header', 'header');
-const TITLE = new Element(HEADER.node, 'h1', 'title');
-TITLE.node.textContent = 'Виртуальная клавиатура';
+    const WRAPPER = new Element(document.body, 'div', 'wrapper');
+    const HEADER = new Element(WRAPPER.node, 'header', 'header');
+    const TITLE = new Element(HEADER.node, 'h1', 'title');
+    TITLE.node.textContent = 'Виртуальная клавиатура';
 
-const MAIN = new Element(WRAPPER.node, 'main', 'main');
-const TEXTAREA = new Element(MAIN.node, 'textarea', 'textarea');
-TEXTAREA.node.setAttribute("rows", 5);
-TEXTAREA.node.setAttribute("columns", 50);
+    const MAIN = new Element(WRAPPER.node, 'main', 'main');
+    const TEXTAREA = new Element(MAIN.node, 'textarea', 'textarea');
+    TEXTAREA.node.setAttribute("rows", 5);
+    TEXTAREA.node.setAttribute("columns", 50);
+    // TEXTAREA.node.setAttribute("autofocus", );
 
-const KEYBOARD = new Element(MAIN.node, 'div', 'keyboard');
+    const KEYBOARD = new Element(MAIN.node, 'div', 'keyboard');
 
-const DESCRIPTION = new Element(MAIN.node, 'p', 'description');
-DESCRIPTION.node.textContent = 'Клавиатура создана в операционной системе Windows';
-const LANGUAGE = new Element(MAIN.node, 'p', 'language');
-LANGUAGE.node.textContent = 'Для переключения языка комбинация: левыe ctrl + alt';
+    const DESCRIPTION = new Element(MAIN.node, 'p', 'description');
+    DESCRIPTION.node.textContent = 'Клавиатура создана в операционной системе Windows';
+    const LANGUAGE = new Element(MAIN.node, 'p', 'language');
+    LANGUAGE.node.textContent = 'Для переключения языка комбинация: левыe ctrl + alt';
 
-KEY_ENG.forEach(keyArr => {
-    let keyLine = new Element(KEYBOARD.node, 'div', 'keyboard__line');
-    keyArr.forEach(key => {
-        let keyButton = new Element(keyLine.node, 'button', 'keyboard__key');
-        keyButton.node.textContent = `${key}`;
+    KEY_ENG.forEach(keyArr => {
+        let keyLine = new Element(KEYBOARD.node, 'div', 'keyboard__line');
+        keyArr.forEach(key => {
+            let keyButton = new Element(keyLine.node, 'button', 'keyboard__key');
+            keyButton.node.textContent = `${key}`;
 
-        switch (key) {
-            case 'Backspace':
-                keyButton.node.classList.add('keyboard__key_very-long')
-                break
+            switch (key) {
+                case 'Backspace':
+                    keyButton.node.classList.add('keyboard__key_very-long')
+                    break
 
-            case 'Tab':
-                keyButton.node.classList.add('keyboard__key_long')
-                break
+                case 'Tab':
+                    keyButton.node.classList.add('keyboard__key_long')
+                    break
 
-            case 'Shift':
-                keyButton.node.classList.add('keyboard__key_long')
-                break
+                case 'Shift':
+                    keyButton.node.classList.add('keyboard__key_long')
+                    break
 
-            case 'Enter':
-                keyButton.node.classList.add('keyboard__key_long')
-                break
+                case 'Enter':
+                    keyButton.node.classList.add('keyboard__key_long')
+                    break
 
-            case 'Caps lock':
-                keyButton.node.classList.add('keyboard__key_long')
-                break
+                case 'Caps lock':
+                    keyButton.node.classList.add('keyboard__key_long')
+                    break
 
-            case 'Space':
-                keyButton.node.classList.add('keyboard__key_extra-long')
-                break
+                case 'Space':
+                    keyButton.node.classList.add('keyboard__key_extra-long')
+                    break
 
-        }
+            }
+        })
+
+
     })
 
+}
 
-})
+window.addEventListener('DOMContentLoaded', init)
+
+
